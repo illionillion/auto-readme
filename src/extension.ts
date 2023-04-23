8232e080c2e4561b309a1268c316087b103316d4
yuto ichinose <yuto.0526.sano@gmail.com>
2023-04-23 12:00:03 +0900

    ファイル分割完了

--- src/extension.ts
+++ src/extension.ts
@@ -1,186 +1,17 @@
-import { readFileSync, writeFile } from "fs";
 import * as vscode from "vscode";
-import {
-  ChatCompletionRequestMessageRoleEnum,
-  Configuration,
-  OpenAIApi,
-} from "openai";
-import { AxiosError } from "axios";
-
-
-function isAxiosError(error: unknown): error is AxiosError {
-  return (error as AxiosError)?.isAxiosError === true;
-}
+import { OpenAIApi } from "openai";
+import { create_readme } from "./commands/create-readme";
+import { set_api_key } from "./commands/set-api-key";
 
 let openai: OpenAIApi | undefined = undefined;
 
-export const generateReadme = async (
-  content: string,
-  model = "gpt-3.5-turbo",
-  role: ChatCompletionRequestMessageRoleEnum = "user"
-) => {
-  try {
-    const response = await openai?.createChatCompletion({
-      model: model,
-      messages: [{ role: role, content: content }],
-    });
-
-    const answer = response?.data.choices[0].message?.content;
-    return { success: true, content: answer };
-  } catch (error) {
-    if (isAxiosError(error) && error?.response?.status === 403) {
-      return {
-        success: false,
-        content: "An error occurred during the request. Please try again.",
-      };
-    } else {
-      return {
-        success: false,
-        content: "An error occurred during the request. Plese check your API key and model",
-      };
-    }
-  }
-};
-/**
- * APIキーの保存
- * @param yourKey
- */
-const save_api_key = async (yourKey: string) => {
-  await vscode.workspace
-    .getConfiguration("create-readme-openai")
-    .update("apiKey", yourKey, vscode.ConfigurationTarget.Global);
-  vscode.window.showInformationMessage("API Key saved to settings.");
-};
-
 export function activate(context: vscode.ExtensionContext) {
   /**
-   * README作成A
+   * README作成
    */
   const create = vscode.commands.registerCommand(
     "create-readme-openai.create-readme",
-    async () => {
-      // APIキーを設定から取得
-      let yourKey = vscode.workspace
-        .getConfiguration("create-readme-openai")
-        .get("apiKey") as string | undefined;
-
-      // APIキーが設定されていない場合、実行時に入力を求める
-      if (!yourKey) {
-        yourKey = await vscode.window.showInputBox({
-          prompt: "Enter your OpenAI API Key.",
-        });
-
-        // 入力がキャンセルされた場合
-        if (!yourKey) {
-          vscode.window.showErrorMessage(
-            "No API key entered! Please set your OpenAI API key in the settings or enter it when prompted."
-          );
-          return;
-        } else {
-          // 入力されたAPIキーを設定に保存する
-          await save_api_key(yourKey);
-        }
-      }
-
-      // モデル名を設定から取得
-      let modelName = vscode.workspace
-        .getConfiguration("create-readme-openai")
-        .get("model") as string | undefined;
-
-      // モデル名が設定されていない場合、実行時に選択を求める
-      if (!modelName) {
-        const modelQuickPickItems: vscode.QuickPickItem[] = [
-          {
-            label: "gpt-3.5-turbo",
-            description: "GPT-3.5 Turbo model",
-          },
-          {
-            label: "gpt-4",
-            description: "gpt-4 model",
-          },
-        ];
-
-
-        const selectedModel = await vscode.window.showQuickPick(
-          modelQuickPickItems,
-          {
-            placeHolder: "Select your OpenAI model.",
-          }
-        );
-
-        // 選択がキャンセルされた場合
-        if (!selectedModel) {
-          vscode.window.showErrorMessage(
-            "No model selected! Please set your OpenAI model in the settings or select it when prompted."
-          );
-          return;
-        } else {
-          // 選択されたモデル名を設定に保存する
-          modelName = selectedModel.label;
-          await vscode.workspace
-            .getConfiguration("create-readme-openai")
-            .update("model", modelName, vscode.ConfigurationTarget.Global);
-
-          vscode.window.showInformationMessage("Model selected and saved to settings.");
-        }
-      }
-
-
-      const configuration = new Configuration({
-        apiKey: yourKey,
-      });
-      openai = new OpenAIApi(configuration);
-
-      const fileName = "README.md";
-      const exportFilePath = `${vscode.workspace.workspaceFolders?.[0].uri.path.substring(
-        1
-      )}/${fileName}`;
-
-      const options = {
-        canSelectMany: false,
-        openLabel: "Select",
-        filters: {
-          all_files: ["*"],
-        },
-      };
-      const targetFileUri = await vscode.window.showOpenDialog(options);
-      if (!targetFileUri) {
-        vscode.window.showErrorMessage("No file selected!");
-        return;
-      }
-      const targetfilePath = targetFileUri[0].fsPath;
-      vscode.window.showInformationMessage(`Selected file: ${targetfilePath}`);
-      const filecontent = readFileSync(targetfilePath, "utf-8");
-
-      const content = `以下のソースコードのREADMEを日本語で作成してください。\n${filecontent}`;
-      // Progress message
-      vscode.window.withProgress(
-        {
-          location: vscode.ProgressLocation.Notification,
-          title: "Creating README...",
-          cancellable: false,
-        },
-        async (progress) => {
-          progress.report({ increment: 0 });
-          const result = await generateReadme(content, modelName);
-          if (result.success) {
-            writeFile(exportFilePath, result.content ?? "", (err) => {
-              if (err) {
-                vscode.window.showErrorMessage(
-                  `Failed to create file: ${err.message}`
-                );
-                return;
-              }
-              vscode.window.showInformationMessage(
-                `File ${fileName} created successfully!`
-              );
-            });
-          } else {
-            vscode.window.showErrorMessage(result.content ?? "");
-          }
-        }
-      );
-      }
+    async () => await create_readme(openai)
   );
 
   /**
@@ -188,18 +19,16 @@ export function activate(context: vscode.ExtensionContext) {
    */
   const setAPIKey = vscode.commands.registerCommand(
     "create-readme-openai.set-api-key",
-    async () => {
-      const yourKey = await vscode.window.showInputBox({
-        prompt: "Enter your OpenAI API Key.",
-      });
-      await save_api_key(yourKey ?? "");
-    }
+    set_api_key
   );
 
   context.subscriptions.push(create); // イベント追加
   context.subscriptions.push(setAPIKey); // イベント追加
+
+  console.log('TEST!!');
+  
 }
 
 // This method is called when your extension is deactivated
 
-export function deactivate() { }
+export function deactivate() {}
